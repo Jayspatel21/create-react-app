192f2e7ff7e7dfad7efb3e0c697ca4e14e2a67fb
jayspatel21 <jayspatel2153@gmail.com>
2024-12-09 17:20:55 +0530

    add count path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const storedCount = localStorage.getItem('count');
+    if (storedCount) {
+      setCount(parseInt(storedCount, 10));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('count', count);
+  }, [count]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          You clicked {count} times
+          <button onClick={() => setCount(count + 1)}>
+            Click me
+          </button>
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,3 +43,5 @@ function App() {
 }
 
 export default App;
+
+
