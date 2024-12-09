66913619f5a27fa93373ca8068561735f7cf1554
jayspatel21 <jayspatel2153@gmail.com>
2024-12-09 17:13:37 +0530

    added posthog

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
+import { PostHogProvider} from 'posthog-js/react'
+
+const options = {
+  api_host: "https://us.i.posthog.com",
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PostHogProvider 
+      apiKey={'phc_cOlskuqwCM4PPtUMy19lYschP45UP4WdHI6djKMRc6A'}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
+);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
